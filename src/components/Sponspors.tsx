9a0da45e3a5f08a7e8255f3d5e4b3c731b5e55ac
Sunil H N <sunilh.n27@gmail.com>
2023-12-29 00:56:06 +0530

    added Sponspors component

---
+++ src/components/Sponspors.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Sponspors() {
+  return (
+    <section className="w-[81rem] h-[12rem] shadow-2xl border border-red-500 flex justify-center items-center mx-auto -mt-16 bg-white rounded-lg ">
+      <div className="flex justify-center items-center">
+        <h1 className="font-bold text-3xl">Trusted by</h1>
+      </div>
+    </section>
+  );
+}
