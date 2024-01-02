7c345ba020ff370a9a627944484dc3b066dc25e2
Sunil H N <sunilh.n27@gmail.com>
2024-01-02 23:45:04 +0530

    updated Footer  component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,33 +1,7 @@
-import Image,{ StaticImageData } from "next/image";
-import React from "react";
-import VectorImg3 from '@/app/asserts/Logo.png';
+import React from 'react'
 
-
-interface ServiceCardProps {
-  image: StaticImageData;
-  title: string;
-  para: string;
-}
-
-const ServiceCard: React.FC<ServiceCardProps> = ({ image, title, para }) => {
+export default function Footer() :JSX.Element {
   return (
-      <div className='flex flex-col'>
-        <div className='shadow-2xl w-[26rem] h-[19rem]  text-center py-12 rounded-lg'>
-          <Image src={image} alt='pic' className='mx-auto mb-6' />
-          <h1 className='font-bold text-[1.5rem] mb-2'>{title}</h1>
-          <p className='w-[21rem] mx-auto text-gray-500'>{para}</p>
-        </div>
-      </div>
-  );
-};
-
-export default function Footer(): JSX.Element{
-  return (<></>
-    // <section className="">
-    //   <div>
-    //     <ServiceCard image={VectorImg3} title="title" para="Para"/>
-    //   </div>
-    // </section>
-  );
+    <div>Footer</div>
+  )
 }
-
