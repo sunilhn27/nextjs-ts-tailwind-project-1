2e99371c76bf26c593588a8ac5fc20cd9247498f
Sunil H N <sunilh.n27@gmail.com>
2024-01-04 00:16:05 +0530

    added bg image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import BgImg from '@/app/asserts/svgexport-5.png'
+import WhiteLogo from '@/app/asserts/white_logo.png'
 
-export default function Footer() :JSX.Element {
+export default function Footer(): JSX.Element {
+  const sectionStyle = {
+    backgroundImage: `url(${BgImg.src})`, 
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    // Add other styles as needed
+  };
+
   return (
-    <div>Footer</div>
-  )
+    <section className="w-full h-[72rem] absolute border-2 border-red-600" style={sectionStyle} >
+      <div className="">Plain Text</div>
+      safdsv
+      <div>dsfdsv</div>
+    </section>
+    
+  );
+  
 }
