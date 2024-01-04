f5892b94ce1606120110b05ea3c9583f9b077173
Sunil H N <sunilh.n27@gmail.com>
2024-01-05 00:11:14 +0530

    completed Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ import PlayStore from "@/app/asserts/svgexport-2.png";
 import AppleStore from "@/app/asserts/svgexport-3.png";
 import Image from "next/image";
 import WhiteLogo from "@/app/asserts/white_logo.png";
+import Link from "next/link";
 
 export default function Footer(): JSX.Element {
   const sectionStyle: React.CSSProperties = {
@@ -31,32 +32,47 @@ export default function Footer(): JSX.Element {
 
   return (
     <section
-      className="w-full h-[60rem] flex flex-col justify-center items-center relative border-2 border-red-600 mt-32 text-red-700"
+      className="w-full h-[60rem] flex flex-col justify-center items-center relative  mt-32 "
       style={sectionStyle}
     >
-      <div className="flex justify-center items-center mt-48">
-        <div className="grid grid-cols-5">
-          <div className="flex flex-col border-2 border-red-500">
+      <div className="flex justify-center gap-24 items-center mt-60">
+          <div className="flex flex-col">
             <Image src={WhiteLogo} alt="whitelogo" className="mb-4" />
-            <h3>Get the future you want</h3>
-            <p className="w-[21rem]">
+            <h3 className="mb-4 text-2xl text-gray-300">
+              Get the future you want
+            </h3>
+            <p className="w-[22rem]">
               We offer great advice, tools and guides to help you plan your best
               future for free.
             </p>
           </div>
-          <div className="border-2 border-red-500">
-            <Image src={WhiteLogo} alt="whitelogo" />
+          <div className="flex flex-col text-gray-300 space-y-2">
+            <Link href={"/"} className="font-bold">Products</Link>
+            <Link href={"/"}>Features</Link>
+            <Link href={"/"}>Templates</Link>
+            <Link href={"/"}>Pricing</Link>
+            <Link href={"/"}>Security</Link>
           </div>
-          <div className="border-2 border-red-500">
-            <Image src={WhiteLogo} alt="whitelogo" />
+          <div className="flex flex-col text-gray-300 space-y-2">
+            <Link href={"/"} className="font-bold">Company</Link>
+            <Link href={"/"}>About Us</Link>
+            <Link href={"/"}>Press & Media</Link>
+            <Link href={"/"}>Careers</Link>
+            <Link href={"/"}>Ambassadors</Link>
           </div>
-          <div className="border-2 border-red-500">
-            <Image src={WhiteLogo} alt="whitelogo" />
+          <div className="flex flex-col text-gray-300 space-y-2">
+            <Link href={"/"} className="font-bold">Support</Link>
+            <Link href={"/"}>FAQ</Link>
+            <Link href={"/"}>Terms & Conditions</Link>
+            <Link href={"/"}>Privacy Policy</Link>
+            <Link href={"/"}>Customer Stories</Link>
           </div>
-          <div className="border-2 border-red-500">
-            <Image src={WhiteLogo} alt="whitelogo" />
+          <div className="flex flex-col text-gray-300 space-y-2">
+            <Link href={"/"} className="font-bold">Connect</Link>
+            <Link href={"/"}>support@pagebulb.com</Link>
+            <Link href={"/"}>+234 903 0000 857</Link>
+            <Link href={"/"}>Follow Us</Link>
           </div>
-        </div>
       </div>
       <div style={logoStyle} className="w-[70rem] h-[30rem] text-white top-3">
         <div className="flex justify-center items-center my-auto py-16">
